2f8b2dd4956de1558d8e392f2bcb6933f064bfff
Sagar <sagarpuniyani@gmail.com>
2024-07-30 14:46:02 +0530

    add: DB setup

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ProfileForm from '@/components/forms/profileform'
+
+type Props = {}
+
+const Settings = (props: Props) => {
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+                <span>Settings</span>
+            </h1>
+            <div className="flex flex-col gap-10 p-6">
+                <div>
+                    <h2 className="text-2xl font-bold">User Profile</h2>
+                    <p className="text-base text-white/50">
+                        Add or update your information
+                    </p>
+                </div>
+            {/* //todod: Add the user */}
+                <ProfileForm user={undefined} />
+            </div>
+        </div>
+    )
+}
+
+
+export default Settings 
